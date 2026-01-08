85d1db046c24e97248568a51288507979953901b
Harry-231 <singhkarandeep214@gmail.com>
2026-01-08 23:49:19 +0530

    changed port

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ async def chat(req: ChatRequest, response: Response):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
